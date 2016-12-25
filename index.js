d56de0dca0470eb2a8e85a7f910cc4e04566b90f
geekingin <geekingin@gmail.com>
2016-12-25 15:32:47 +0800

    initial lib

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by jeffjing on 2016/12/25.
+ */
+module.exports = require('./index.css')
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack')
+var ExtractWebpackPlugin = require('extract-text-webpack-plugin')
+var env = process.env.NODE_ENV.trim()
+
+module.exports = {
+    entry: {
+        'alloy-normalize': './index.js',
+        'alloy-normalize.min': './index.js'
+    },
+    output: {
+        path: './dist/',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: ExtractWebpackPlugin.extract('style-loader', 'css-loader')
+        }]
+    },
+    plugins: [
+        new ExtractWebpackPlugin('[name].css')
+    ]
+}
+//
+// if (env === 'production') {
+//     module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin())
+// }
